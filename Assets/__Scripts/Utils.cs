e0c9a3ad4c6c1df4360a74596165fefefe551a1a
Forclasses <brucec22180@gmail.com>
2025-04-14 14:10:06 -0500

    fixed error
    Aplha is annoying

--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SpeedTree.Importer;
+// using UnityEditor.SpeedTree.Importer;
 using UnityEngine;
 
 public class Utils : MonoBehaviour
